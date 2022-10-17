ac5150c823c615a19f16d4b68aae73c72d713dcf
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2022-10-17 09:53:38 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - Entry point to program
+ *
+ * Return: 0
+ */
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
