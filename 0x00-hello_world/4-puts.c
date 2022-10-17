e476055f87913baac0b2fa43b776197db3d745df
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2022-10-17 10:40:25 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point to program
+ *
+ * main - Entry point to program 
  *
  * Return: 0
-*/
-int main(void) 
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
