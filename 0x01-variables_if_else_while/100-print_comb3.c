e9eded46d07581494425bbc3f58f87594b6023c7
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2022-10-19 06:40:53 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,23 @@
  */
 int main(void)
 {
-	int a;
+	int ones = '0';
+	int tens = '0';
 
-	for (a = 0; a < 10; a++)
+	for (tens = '0'; tens < '9'; tens++)
 	{
-		putchar(a + '0');
-		if (a < 9)
+		for (ones = '0'; ones < '9'; ones++)
 		{
-			putchar(',');
-			putchar(' ');
+			if (!((ones == tens) || (tens > ones)))
+			{
+				putchar(ones);
+				putchar(tens);
+				if (!(ones == '9' && tens == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
