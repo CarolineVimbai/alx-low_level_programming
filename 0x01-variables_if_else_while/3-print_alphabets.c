ee6e1bb8edacd6762fb78b66d67ec62e6fb25b0f
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2022-10-19 03:53:05 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	int e;
+
+	c = 'a';
+	e = 'A';
+	while 
+		(c <='z') {
+			putchar(c);
+			c++;
+		}
+	while 
+		(e <= 'Z') {
+			putchar(e);
+			e++;
+		}
+	putchar('\n');
+	return (0);
+}
