59c636a2e55a9b4ec13951b2eff926cbdda53e03
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2022-10-21 21:11:16 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
+ * main - Print _putchar
  * Return: Always 0
  */
 int main(void)
 {
-	_putchar(' ');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
