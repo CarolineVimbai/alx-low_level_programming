bcc7011c5949cca29f5bcd3e10283b800b8579c9
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2022-10-21 17:41:05 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
  * print_number - prints an integer
  * @n:.input integer parameter
  *
- * Owned by Bwave ICT/ Bright Daniel
+ *Owned by Bwave ICT/ Bright Daniel
  */
 void print_number(int n)
 {
